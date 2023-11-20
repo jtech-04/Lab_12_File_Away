3deb10112c75376f8bd0d4c8537bba70fae66167
jamie <jamiek0414@gmail.com>
2023-11-20 15:46:31 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,78 @@
-import java.io.*;
-import javax.swing.*;
+import java.io.File;
 
-public class Main {
+import java.io.FileNotFoundException;
 
-    public static void main(String[] args) throws IOException {
-        // Create a new JFileChooser object to let the user select a file
-        JFileChooser fileChooser = new JFileChooser();
-        // Show the dialog box and get the user's choice
-        int result = fileChooser.showOpenDialog(null);
-        if (result != JFileChooser.APPROVE_OPTION) {
-            // If the user cancelled, exit the program
-            return; // user cancelled
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+
+public class Main
+
+{
+
+    public static void main(String args[])
+
+    {
+
+
+
+        final JFileChooser file_chooser = new JFileChooser();
+
+
+
+        int returnVal = file_chooser.showOpenDialog(null);
+
+
+
+        File file=file_chooser.getSelectedFile();
+
+
+        int linecount=0;
+
+        int wordcount=0;
+
+        int charactercount=0;
+
+        String line="";
+
+        try {
+
+
+
+            Scanner read=new Scanner(file);
+
+
+
+            while(read.hasNextLine())
+
+            {
+
+                line=read.nextLine();
+
+
+                String words[]=line.split(" ");
+
+
+
+                charactercount=charactercount+line.length();
+
+                wordcount=wordcount+words.length;
+
+                linecount=linecount+1;
+
+            }
+
+        } catch (FileNotFoundException e) {
+
+
+            e.printStackTrace();
+
         }
-        // Get the selected file and its name
-        String fileName = fileChooser.getSelectedFile().getName();
-        // Create a BufferedReader to read the file line by line
-        BufferedReader reader = new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
-        // Initialize counters for lines, words, and characters
-        int lines = 0;
-        int words = 0;
-        int chars = 0;
-        String line = null;
-        // Read each line of the file and count the lines, words, and characters
-        while ((line = reader.readLine()) != null) {
-            //Implement lines counter
-            lines++;
-            // Split the line into words by whitespace and count the number of words
-            String[] wordsArray = line.split("\\s+");
-            words += wordsArray.length;
-            // Count the number of characters in the line
-            chars += line.length();
-        }
-        // Print the summary report to the console
-        System.out.println("File name: " + fileName);
-        System.out.println("Number of lines: " + lines);
-        System.out.println("Number of words: " + words);
-        System.out.println("Number of characters: " + chars);
-        //Close Reader
-        reader.close();
+
+
+
+        System.out.printf("file name: %s \n character count: %d \n word count: %d \n line count: %d \n",file.getName(),charactercount,wordcount,linecount);
 
     }
+
+}
